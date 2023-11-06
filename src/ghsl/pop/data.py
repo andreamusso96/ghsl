1c69d405b9f8c5f1c705f86becdb761837f0980c
andreamusso96 <andreamusso96@gmail.com>
2023-11-06 19:08:19 +0100

    fixed year bug

--- src/ghsl/pop/data.py
+++ src/ghsl/pop/data.py
@@ -15,6 +15,7 @@ class Data:
         if self._data is None or self._year != year:
             raw_data = utils.open_raster_file(config.get_population_raster_file_path(year=year))
             self._data = utils.transform_xarray_into_geopandas(data=raw_data.squeeze(dim='band', drop=True), column_name='population', remove_vals=[-200])
+            self._year = year
         return self._data
 
 
--- src/ghsl/smod/data.py
+++ src/ghsl/smod/data.py
@@ -15,6 +15,7 @@ class Data:
         if self._data is None or self._year != year:
             raw_data = utils.open_raster_file(config.get_settlement_raster_file_path(year=year))
             self._data = utils.transform_xarray_into_geopandas(data=raw_data.squeeze(dim='band', drop=True), column_name='degree_of_urbanization', remove_vals=[-200, enums.DegreeOfUrbanization.WATER.value])
+            self._year = year
         return self._data
 
 
